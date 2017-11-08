03c85a41b37f1fa69f261114319d23529d34e0c2
David Pickart <davidstoreypickart@gmail.com>
2017-11-08 10:15:54 -0600

    Add jsx-a11y

--- react.js
+++ react.js
@@ -2,9 +2,11 @@ module.exports = {
   'extends': [
     './es6.js',
     'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
   ],
   'plugins': [
     'react',
+    'jsx-a11y',
   ],
   'rules': {
     'react/prop-types': [
@@ -12,6 +14,9 @@ module.exports = {
     ],
     'react/no-unused-prop-types': [
       'warn',
+    ],
+    'react/no-unescaped-entities': [
+      'off',
     ]
   },
 }
